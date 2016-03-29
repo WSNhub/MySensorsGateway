63f731ff81a5627fe8242483e0ef988c42781c8a
Alain <alain.maes@telenet.be>
2016-03-29 23:03:18 +0200

    Ethernet interface MUST be the default interface for routing to work properly

--- libraries/W5100/w5100_netif.cpp
+++ libraries/W5100/w5100_netif.cpp
@@ -30,24 +30,43 @@ struct ip_addr netmask   = {0x00000000UL};
 Timer ethTimer;
 void ethTimerHandler()
 {
+    if (netif_default != &w5100_netif)
+        netif_set_default(&w5100_netif);
+
     ethernetif_input(&w5100_netif);
+}
+
+void w5100_show_interfaces()
+{
+    struct netif *netif;
+    Serial.printf("\n\nInterfaces:\n");
+    for(netif = netif_list; netif != NULL; netif = netif->next) {
+        Serial.printf("Found interface %c%c%d\n",
+                      netif->name[0], netif->name[1], netif->num);
+        IPAddress addr(netif->ip_addr.addr);
+        Serial.printf("    IP   %s\n", addr.toString().c_str());
+        IPAddress mask(netif->netmask.addr);
+        Serial.printf("    MASK %s\n", mask.toString().c_str());
+        IPAddress gw(netif->gw.addr);
+        Serial.printf("    GW   %s\n", gw.toString().c_str());
+    }
 }
 
 void w5100_netif_init()
 {
-  wifi_get_macaddr(STATION_IF, w5100_netif_if.address);
+    wifi_get_macaddr(STATION_IF, w5100_netif_if.address);
+    
+    // Add our netif to LWIP (netif_add calls our driver initialization function)
+    if (netif_add(&w5100_netif, &myip_addr, &netmask, &gw_addr, &w5100_netif_if,
+                  ethernetif_init, ethernet_input) == NULL) {
+        Serial.println("netif_add (w5100_netif_init) failed");
+    }
 
-  // Add our netif to LWIP (netif_add calls our driver initialization function)
-  if (netif_add(&w5100_netif, &myip_addr, &netmask, &gw_addr, &w5100_netif_if,
-                ethernetif_init, ethernet_input) == NULL) {
-      Serial.println("netif_add (w5100_netif_init) failed");
-  }
+    netif_set_default(&w5100_netif);
+    //netif_set_up(&w5100_netif);
+    dhcp_start(&w5100_netif);
 
-  //netif_set_default(&w5100_netif);
-  //netif_set_up(&w5100_netif);
-  dhcp_start(&w5100_netif);
-
-  ethTimer.initializeMs(1, ethTimerHandler).start(true);
+    ethTimer.initializeUs(500, ethTimerHandler).start(true);
 }
 
 IPAddress w5100_netif_get_ip()
